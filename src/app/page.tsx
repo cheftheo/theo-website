98f844630f9527f5a506e86a504a6e46dc2a4cbe
Marascu Theodor <marascu.theodor@gmail.com>
2025-06-25 02:48:31 +0300

    added whatido component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
 import  { TextGenerateEffect }  from '../components/text-effect';
 
-import { SocialsPage } from '../components/socials';
+import { SocialsPage } from '@/components/socials';
+import { WhatIdo } from '@/components/whatido';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-
-
 export default function Home() {
   const prop1 = "Tech Engineer who loves innovation"
 
-  
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1> */}
-      <section className="grid grid-cols-2 text-neutral-100 gap-6">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 max-[600px]:p-10">
+      <section className="grid grid-cols-2 text-neutral-100 gap-6 max-[600px]:flex max-[600px]:flex-col max-[600px]:overflow-y-auto">
         <div className="bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col justify-center items-start gap-4">
           <div className="gap-2 font-bold text-2xl">
             Hello there, here is Theo.
@@ -33,7 +29,6 @@ export default function Home() {
 
         <SocialsPage />
 
-
         <section className='flex col-span-2 justify-center'>
           <div className='bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col gap-2 text-neutral-100 w-full max-w-5xl'>
             I'm a young tech enthusiast who enjoys working with systems and finding ways to make them better. I like automating things that people usually do by hand, 
@@ -42,20 +37,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className='flex col-span-2 justify-center'>
-          <div className='bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col gap-2 text-neutral-100 w-full max-w-5xl'>
-            <h2 className='text-xl font-bold'>What I do</h2>
-            <p className='text-neutral-400'>I specialize in creating and maintaining systems that help teams work better. I focus on making things simple, clear, and reliable.</p>
-            <ul className='list-disc pl-5 text-neutral-400'>
-              <li>System Design</li>
-              <li>Automation</li>
-              <li>Monitoring</li>
-              <li>Documentation</li>
-              <li>Collaboration</li>
-              <li>testt????</li>
-            </ul>
-          </div>  
-        </section>
+        <WhatIdo />
 
       </section>
     </main>
---
+++ src/components/whatido.tsx
@@ -0,0 +1,24 @@
+
+export const WhatIdo = () => {
+    return (
+        <section className='flex col-span-2 justify-center'>
+          <div className='bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col gap-2 text-neutral-100 w-full max-w-5xl'>
+            <h2 className='text-xl font-bold'>What I do?</h2>
+            <p className='text-neutral-400'>I specialize in creating and maintaining systems that help teams work better. I focus on making things simple, clear, and reliable. This is my tech stack:</p>
+            <ul className='pl-5 text-neutral-400 flex flex-rows gap-2 flex-wrap text-white'>
+              <li>Networking</li>
+              <li>Active Directory</li>
+              <li>Linux/Windows</li>
+              <li>Python Lua Bash PowerShell</li>
+              <li>Docker</li>
+              <li>K8s</li>
+              <li>AWS Cloud</li>
+              <li>Git Github Actions</li>
+              <li>Grafana Prometheus Loki cAdvisor</li>
+              <li>SQL MongoDB PostgreSQL</li>
+              <li>JS/TS NextJS TailwindCSS</li>
+            </ul>
+          </div>  
+        </section>
+    )
+}
