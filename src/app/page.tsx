8e0a64a1725c082f1ab4df61ae45b5449ff5b419
Marascu Theodor <marascu.theodor@gmail.com>
2025-06-24 03:06:20 +0300

    added features card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import  { TextGenerateEffect }  from '../components/text-effect';
+
+import { SocialsPage } from '../components/socials';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const prop1 = "Tech Engineer who loves innovation"
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1> */}
+      <section className="grid grid-cols-2 text-neutral-100 gap-6">
+        <div className="bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col justify-center items-start gap-4">
+          <div className="gap-2 font-bold text-2xl">
+            Hello there, here is Theo.
+            <TextGenerateEffect className="font-semibold text-neutral-400" words={prop1} />
+          </div>
+
+          <div className='flex flex-row gap-1'>
+            <FontAwesomeIcon icon={faLocationDot} className='size-4'/>
+            <span className='text-neutral-400 text-sm font-semibold'>Based in</span>
+            <span className='text-neutral-100 text-sm font-semibold'>Bucharest</span>
+          </div>
+
+          <button className="p-1 px-3 border border-solid rounded-xl text-neutral-400 items-end self-end hover:text-neutral-100 transition-all ">Contact me</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <SocialsPage />
+
+
+        <section className='flex col-span-2 justify-center'>
+          <div className='bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col gap-2 text-neutral-100 w-full max-w-5xl'>
+            I'm a young tech enthusiast who enjoys working with systems and finding ways to make them better. I like automating things that people usually do by hand, 
+            and making sure everything runs smoothly. I'm always curious and open to learning new tools that help teams work faster and easier. 
+            For me, it's important to build things that are simple, clear, and reliable.
+          </div>
+        </section>
+
+        <section className='flex col-span-2 justify-center'>
+          <div className='bg-zinc-800 p-6 rounded-lg shadow-md flex flex-col gap-2 text-neutral-100 w-full max-w-5xl'>
+            <h2 className='text-xl font-bold'>What I do</h2>
+            <p className='text-neutral-400'>I specialize in creating and maintaining systems that help teams work better. I focus on making things simple, clear, and reliable.</p>
+            <ul className='list-disc pl-5 text-neutral-400'>
+              <li>System Design</li>
+              <li>Automation</li>
+              <li>Monitoring</li>
+              <li>Documentation</li>
+              <li>Collaboration</li>
+            </ul>
+          </div>  
+        </section>
+
+      </section>
+    </main>
   );
 }
