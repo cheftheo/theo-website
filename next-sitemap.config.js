a5246b55e2379ddcf2260794643ba1c19a96743c
Marascu Theodor <marascu.theodor@gmail.com>
2025-07-02 16:40:23 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://theo-network.online', // your full domain
+  generateRobotsTxt: true, // (optional) generates robots.txt file
+  sitemapSize: 7000,
+  changefreq: 'monthly',
+  priority: 0.7,
+};
