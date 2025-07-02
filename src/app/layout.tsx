799e8d62efd053d0fa863fa7fa675831cd91c52d
Marascu Theodor <marascu.theodor@gmail.com>
2025-07-02 17:06:05 +0300

    added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,14 +25,19 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-       <meta name="description" content="I'm Theo, a wannabe engineer. Explore my portfolio, projects, and experience." />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://theo-network.online/" />
+      <meta name="description" content="I'm Theo, a wannabe engineer. Explore my portfolio, projects, and experience." />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://theo-network.online/" />
 
-        <meta name="author" content="Theodor Marascu" />
-        <meta property="og:title" content="Theo's network" />
-        <meta property="og:description" content="I'm Theo, a wannabe engineer. Check out my work." />
-        <meta property="og:url" content="https://theo-network.online/" />
+      <meta name="author" content="Theodor Marascu" />
+      <meta property="og:title" content="Theo's network" />
+      <meta property="og:description" content="I'm Theo, a wannabe engineer. Check out my work." />
+      <meta property="og:url" content="https://theo-network.online/" />
+
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-900`}>
         {children}
